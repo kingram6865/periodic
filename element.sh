bf13a157676433a3d848d82304f2e4ef320e79ba
Ken Ingram <ken.ingram@gmail.com>
2025-05-01 12:52:36 +0000

    refactor: conditional for symbol needs reworking.

--- element.sh
+++ element.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Please provide an element as an argument."
+
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 SQL=''
 SQL1="select 
@@ -19,20 +19,26 @@ SQL1="select
 if [[ $1 =~ ^[0-9]+$ ]]; then
   SQL2=" where properties.atomic_number = $1;"
   SQL="$SQL1 $SQL2"
-elif [[ -n "$1" ]] && [[ ${#1} -eq "2" ]]; then
+elif [[ -n "$1" ]] && [[ $1 =~ ^[a-zA-Z]{1,2}$ ]]; then
   SQL2=" where elements.symbol = '$1';"
   SQL="$SQL1 $SQL2"
 elif [[ -n "$1" ]] && [[ ${#1} -gt "2" ]]; then
   SQL2=" where elements.name = '$1';"
   SQL="$SQL1 $SQL2"
-#else
-#  SQL="SELECT * FROM properties;"
+elif [[ -z "$1" ]]; then
+  SQL=""
 fi
 
-RESULT=$($PSQL "$SQL")
+if [[ -z "$SQL" ]]; then
+  RESULT="None"
+else
+  RESULT=$($PSQL "$SQL")
+fi
 
-if [[ -z "$RESULT" ]]; then
+if [[ "$RESULT" == 'None' ]]; then
+  echo "Please provide an element as an argument."
+elif [[ -z "$RESULT" ]]; then
   echo "I could not find that element in the database."
 else
-  echo "$RESULT" | awk -F'|' '{print "The element with atomic number " $2 " is " $7 " (" $6 "). It''''s a " $1 " with a mass of " $3 " amu. " $7 " has a melting point of " $4 " celsius and a boiling point of " $5 " celsius."}'
+  echo "$RESULT" | awk -F'|' '{print "The element with atomic number " $2 " is " $7 " (" $6 "). It\047s a " $1 ", with a mass of " $3 " amu. " $7 " has a melting point of " $4 " celsius and a boiling point of " $5 " celsius."}'
 fi
